a135d2f9496568a023f52444a8cf3b6b3b494846
ZhengyuanWang <43016364+ZhengyuanWang@users.noreply.github.com>
2019-03-24 16:06:55 -0400

    Add files via upload

---
+++ firewall.py
@@ -0,0 +1,28 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
+from collections import namedtuple
+import os
+
+log = core.getLogger()
+
+class Firewall (EventMixin):
+
+    def __init__ (self):
+        self.listenTo(core.openflow)
+        log.debug("Enabling Firewall Module")
+
+    def _handle_ConnectionUp (self, event):
+        ''' Add your logic here ... '''
+	msg = of.ofp_flow_mod()
+	msg2 = of.ofp_flow_mod()
+	msg.match.dl_src = EthAddr("00:00:00:00:00:02")
+	connection.send(msg)
+	msg2.match.dl_dst = EthAddr("00:00:00:00:00:03")
+	connection.send(msg2)	
+        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+
+def launch ():
+    core.registerNew(Firewall)
